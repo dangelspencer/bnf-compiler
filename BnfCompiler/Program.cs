6e49c3dda3908cc47b00ee7e9dfe1d3bc7f74981
Spencer Dangel <dangelspencer@gmail.com>
2018-01-19 22:06:41 -0500

    Reads contents of a file given from the command line

--- BnfCompiler/Program.cs
+++ BnfCompiler/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace BnfCompiler
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // verify input arguments
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Error: No filename given. \nPlease use 'dotnet run <file_name>'");
+                return 1;
+            }
+            if (args.Length > 1) 
+            {
+                System.Console.WriteLine("Error: Too many arguments.\nPlease use 'dotnet run <file_name>'");
+                return 1;
+            }
+
+            // read file
+            System.Console.WriteLine("Processing file: " + args[0]);
+            var file = args[0];
+
+            string contents = "";
+            try
+            {
+                //read contents of file
+                FileStream fileStream = new FileStream(file, FileMode.Open);
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    contents = reader.ReadToEnd();
+                }
+            } 
+            catch (System.IO.FileNotFoundException) 
+            {
+                // catch error where file doesn't exist
+                System.Console.WriteLine("Error: Could not read file. Please give the absolute path of the file.");
+                return 1;
+            }
+
+            //test to check file contents
+            for (int i = 0; i < contents.Length; i++)
+            {
+                System.Console.WriteLine(i + ": " + contents.Substring(i, 1));
+            }
+
+            return 0;
         }
     }
 }
