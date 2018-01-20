bcb670424c59cdc3d65a6e5f373fb38e3a477299
Spencer Dangel <dangelspencer@gmail.com>
2018-01-19 21:32:02 -0500

    Inital commit

---
+++ BnfCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BnfCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
